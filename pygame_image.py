04cf55de177795f4352c3037375a44a4317f2830
belltree <c0a22087e5@edu.teu.ac.jp>
2023-09-26 16:44:38 +0900

    演習問題終了

--- pygame_image.py
+++ pygame_image.py
@@ -7,26 +7,37 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    gb_img = pg.transform.flip(bg_img,True,False)
+    bg_ls = [bg_img,gb_img]
     tmr = 0
     #こうかとん画像読み込み
     original_img = pg.image.load("ex01/fig/3.png")
     #動きこうかとん
     kouka_hanten_img = pg.transform.flip(original_img,True,False)
-    kouka_naname_img = pg.transform.rotozoom(kouka_hanten_img,10,1.0)
+    kouka_2_img = pg.transform.rotozoom(kouka_hanten_img,5,1.0)
+    kouka_3_img = pg.transform.rotozoom(kouka_hanten_img,10,1.0)
 
-    kouka_img=[kouka_hanten_img,kouka_naname_img]
-
+    kouka_img=[kouka_hanten_img,kouka_2_img,kouka_3_img]
+    x=0
+    x2=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        #背景
+        if x==-2400:
+            x=800
+        screen.blit(bg_img, [x, 0])
 
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(kouka_img[tmr%2],[300,200])
+        if x2==-3200:
+            x2==0
+        screen.blit(gb_img,[x2+1600,0])
+        #こうかとん
+        screen.blit(kouka_img[tmr%3],[300,200])
         pg.display.update()
-        tmr += 1
-        if tmr==1600:
-            tmr=0        
-        clock.tick(10)
+        x -= 1
+        x2-= 1
+        tmr += 1     
+        clock.tick(100)
 
 
 if __name__ == "__main__":
